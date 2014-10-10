9953dfae6e1a1e5e31a2e42fb6748f4d442483da
Tim Hagmann <tim.hagmann@gmail.com>
2014-10-10 11:58:33 +0200

    add css

--- 00_main.R
+++ 00_main.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
-# Purpose:         Responsive SVG
-# Date:            2014-08-15       
+# Purpose:         HTML Experiments
+# Date:            2014-10-10       
 # Author:          http://timelyportfolio.blogspot.com, Tim Hagmann
 # Data Used:       
 # Packages Used:   
@@ -16,76 +16,5 @@ download.file(url="https://rawgit.com/greenore/initR/master/init.R",
 ## Source File
 source("01_initialize.R")
 source("02_load.R")
-
-respXML <- function( svg_xml, height = NULL, width = "100%", print = T, ... ){  
-  svg <- structure(
-    ifelse(
-      length(getDefaultNamespace(svg_xml)) > 0
-      ,getNodeSet(svg_xml,"//x:svg", "x")
-      ,getNodeSet(svg_xml,"//svg")
-    )
-    ,class="XMLNodeSet"
-  )
-  
-  xmlApply(
-    svg
-    ,function(s){
-      a = xmlAttrs(s)
-      removeAttributes(s)
-      xmlAttrs(s) <- a[-(1:2)]
-      xmlAttrs(s) <- c(
-        style = paste0(
-          "height:100%;width:100%;"
-        )
-      )
-    }
-  )
-  
-  svg <- HTML( saveXML( svg_xml) )
-  
-  svg <- tags$div(
-    style = paste(
-      sprintf('width:%s;',width)
-      ,ifelse(!is.null(height),sprintf('height:%s;',height),"")
-    )
-    ,svg
-  )
-  
-  if(print) html_print(svg) 
-  
-  return( invisible( svg ) )
-}
-
-html_print(attachDependencies(
-  tagList(
-    respXML( 
-      svgPlot(
-        dotchart(
-          t(VADeaths)
-          , xlim = c(0,100)
-          , main = "Death Rates in Virginia - 1940"
-        )
-      )
-      , height = "100%"
-      , print = F
-    )
-    , tags$script(
-      HTML(
-        '
-        var svg = d3.selectAll("svg");
-        svg.select("g[id*= \'surface\']").call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
-        
-        function zoom() {
-          svg.select("g[id*= \'surface\']").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-        }
-        '
-      )
-    )
-  )
-  ,htmlDependency(
-    name="d3"
-    ,version="3.0"
-    ,src=c("href"="http://d3js.org/")
-    ,script="d3.v3.js"
-  )
-))
+source("03_responsiveSVG.R")
+source("04_rCSS.R")
