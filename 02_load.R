1b0a0dfd731522238520e0f34d5f321c878b13ac
Tim Hagmann <tim.hagmann@gmail.com>
2014-10-15 01:51:27 +0200

    use packrat

--- 02_load.R
+++
@@ -1,8 +0,0 @@
-## Load Dependencies
-# Bioconductor
-requiredPackages <- c('SVGAnnotation')
-packagesBioconductor(requiredPackages, update=F)
-
-# CRAN
-requiredPackages <- c("htmltools", "pipeR")
-packagesCRAN(requiredPackages, update=F)
