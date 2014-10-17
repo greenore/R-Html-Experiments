20cbd3b789bc769711f5a47a3c81a31cfb5ce96d
Tim Hagmann <tim.hagmann@gmail.com>
2014-10-17 04:46:14 +0200

    add leflet.R

--- 00_main.R
+++ 00_main.R
@@ -2,14 +2,14 @@
 # Purpose:         HTML Experiments
 # Date:            2014-10-10
 # Author:          http://timelyportfolio.blogspot.com, Tim Hagmann
-# Machine:         X10004122 (Win 32)
 # Notes:           WINDOWS: In order for it to work, RTools() has to be installed
 # R Version:       R version 3.1.1 -- "Sock it to Me"
 ################################################################################
 
-## Download init File
+## Download init File (with helper functions)
 download.file(url="https://rawgit.com/greenore/initR/master/init.R",
-              destfile="01_init.R", method="wget")
+              destfile="01_init.R",
+              method=ifelse(Sys.info()["sysname"][[1]] == "Linux", "wget", "auto"))
 
 ## Source File
 update_packages <- FALSE
@@ -17,3 +17,4 @@ source("01_init.R")
 source("02_load.R")
 source("03_responsiveSVG.R")
 source("04_rCSS.R")
+source("05_leafletR.R")
--- 02_load.R
+++ 02_load.R
@@ -7,4 +7,10 @@ packagesBioconductor(requiredPackages, update=update_packages)
 requiredPackages <- c("htmltools", "pipeR")
 packagesCRAN(requiredPackages, update=update_packages)
 
+# Github
+requiredPackages <- c("leafletR")
+packagesGithub(requiredPackages, repo_name="chgrl",
+               proxy_url="webproxy.balgroupit.com", port=3128,
+               update=update_packages)
+
 rm(requiredPackages)
